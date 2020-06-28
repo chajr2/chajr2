f8928f99fdd91ed3ee3f1ea56996d0fd6fe7ad5c
David Stinson <mail@davidstinson.dev>
2020-06-28 16:20:33 -0500

    build(feat): display version number

--- chajr.js
+++ chajr.js
@@ -1,1 +1,13 @@
-console.log("hello world")
+const { Command } = require("commander");
+
+const program = new Command();
+program.version("0.0.1");
+
+program.option("-d, --debug", "Show debugging logs");
+program.option("-V, --version", "Show the current version");
+
+program.command("help").action(() => program.help());
+
+program.parse(process.argv);
+
+if (!program.args.length) program.help();
